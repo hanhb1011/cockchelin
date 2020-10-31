202e5b69304be1fbb6a45385d304185714605824
hanhb1011 <1hanhb@naver.com>
2020-10-31 14:09:01 +0900

    add RecipeSearchViewModel

---
+++ cockchelin/ViewModel/RecipeSearchViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  RecipeSearchViewModel.swift
+//  cockchelin
+//
+//  Created by 한효병 on 2020/10/31.
+//
+
+import Foundation
+
+
+
+class RecipeSearchViewModel: ObservableObject {
+    
+    
+    
+    
+    
+    
+    
+    
+}
