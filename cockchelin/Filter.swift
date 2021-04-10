f7b2c96702aa4688808d83b964ebac157a0df2b4
qhrlq <qhrlq@naver.com>
2021-04-10 19:04:43 +0900

    filterview

--- cockchelin/Filter.swift
+++ cockchelin/Filter.swift
@@ -11,7 +11,7 @@ import Foundation
 class Filter: ObservableObject {
     
     @Published var minDegree: Double = 0.0
-    @Published var maxDegree: Double = 100.0
+    @Published var maxDegree: Double = 50.0
     
     @Published var ingredients: [String] = []
     
--- cockchelin/View/FilterView.swift
+++ cockchelin/View/FilterView.swift
@@ -15,16 +15,15 @@ struct Filterview : View{
     var filter: Filter
     @State var width : CGFloat
     @State var width1 : CGFloat
-    var totalWidth = UIScreen.main.bounds.width-60
+    let totalWidth: CGFloat = 200
     
     @State var Colors = [
         
-        FilterItem(title: "붉은 빛", checked: true),
-        FilterItem(title: "푸른 빛", checked: true),
-        FilterItem(title: "노란 빛", checked: true),
-        FilterItem(title: "다채로운 색상 ", checked: true),
-        FilterItem(title: "무색", checked: true)
-
+        FilterItem(title: "붉음", checked: true, color: "red"),
+        FilterItem(title: "노란", checked: true, color: "yellow"),
+        FilterItem(title: "초록", checked: true, color: "green"),
+        FilterItem(title: "푸름", checked: true, color: "blue"),
+        FilterItem(title: "기타", checked: true, color: "gray")
         ]
     
     @State var showFilter = false
@@ -44,6 +43,104 @@ struct Filterview : View{
     var body: some View{
         ScrollView{
         VStack{
+   
+            
+            VStack{
+                
+                HStack{
+                    Text("색상")
+                        .fontWeight(.bold  )
+                        .foregroundColor(.black)
+                    
+                    Spacer()
+                   
+                        Text("적용")
+                            .fontWeight(.heavy)
+                            
+                }.padding([.horizontal,.top])
+                .padding(.bottom, 10)
+                HStack{
+                ForEach(Colors){filter in
+                    CardView(filter:filter)
+                }
+                }
+            }
+            .padding(.bottom,10)
+            .padding(.top,10)
+            .background(Color.white)
+            //ColorRadio
+        
+            VStack {
+                HStack{
+                    VStack{
+                Text("도수")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    
+                Text("\(self.getValue(val: round(50*self.width/self.totalWidth)))-\(self.getValue(val:round(50*self.width1/self.totalWidth)))도")
+                    .fontWeight(.bold)
+                    }//Text
+                    
+                    Spacer()
+                    
+                    ZStack(alignment:.leading){
+                        
+                        Rectangle()
+                            .fill(Color.black.opacity(0.20))
+                            .frame(width: self.totalWidth, height:6)
+                        
+                        Rectangle()
+                            .fill(Color.black)
+                            .frame(width: self.width1 - self.width, height: 6)
+                            .offset(x: self.width)
+                        
+                        HStack(spacing: 0){
+                            
+                            Circle()
+                                .fill(Color.black)
+                                .frame(width: 18, height: 18)
+                                .offset(x: self.width)
+                                .gesture(
+                                
+                                    DragGesture()
+                                        .onChanged({
+                                            (value) in
+                                            
+                                            if value.location.x>=0 && value.location.x<=self.width1{
+                                                self.width = value.location.x
+                                                self.filter.minDegree = Double(100 * self.width / self.totalWidth)/2
+                                            }
+                                        })
+                                    
+                                )
+                            
+                            Circle()
+                                .fill(Color.black)
+                                .frame(width: 18, height: 18)
+                                .offset(x: self.width1 - self.totalWidth/10)
+                                .gesture(
+                                
+                                    DragGesture()
+                                        .onChanged({
+                                            (value) in
+                                            
+                                            print("\(filter.maxDegree)")
+                                            
+                                            if value.location.x<=self.totalWidth && value.location.x>=self.width{
+                                                self.width1 = value.location.x
+                                                self.filter.maxDegree = Double(100 * self.width1 / self.totalWidth)/2
+                                            }
+                                        })
+                                    
+                                )
+                        }
+                    
+                    }.padding(25) //Rangeslider
+                    
+                }
+                }
+            .padding()
+            
             HStack {
                 Text("재료 선택")
                     .bold()
@@ -88,104 +185,6 @@ struct Filterview : View{
                 Spacer()
                 
             }
-            
-            
-            //GlassType RadioButton
-            VStack{
-                
-                HStack{
-                    Text("색상")
-                        .font(.title)
-                        .fontWeight(.bold  )
-                        .foregroundColor(.black)
-                    
-                    Spacer()
-                   
-                        Text("적용")
-                            .fontWeight(.heavy)
-                            
-                }.padding([.horizontal,.top])
-                .padding(.bottom, 10)
-                
-                ForEach(Colors){filter in
-                    CardView(filter:filter)
-                    
-                }
-            }
-            .padding(.bottom,10)
-            .padding(.top,10)
-            .background(Color.white)
-            
-            //Degree RangeSlider
-            
-            
-            
-            VStack {
-                HStack{
-                Text("도수")
-                    .font(.title)
-                    .fontWeight(.bold)
-                
-                    Spacer()
-                    
-                Text("\(self.getValue(val: 100*self.width/self.totalWidth))-\(self.getValue(val:100*self.width1/self.totalWidth))도")
-                    .fontWeight(.bold)
-                    
-                }
-                ZStack(alignment:.leading){
-                    
-                    Rectangle()
-                        .fill(Color.black.opacity(0.20))
-                        .frame(height:6)
-                    
-                    Rectangle()
-                        .fill(Color.black)
-                        .frame(width: self.width1 - self.width, height: 6)
-                        .offset(x: self.width + 18)
-                    
-                    HStack(spacing: 0){
-                        
-                        Circle()
-                            .fill(Color.black)
-                            .frame(width: 18, height: 18)
-                            .offset(x: self.width)
-                            .gesture(
-                            
-                                DragGesture()
-                                    .onChanged({
-                                        (value) in
-                                        
-                                        if value.location.x>=0 && value.location.x<=self.width1{
-                                            self.width = value.location.x
-                                            self.filter.minDegree = Double(100 * self.width / self.totalWidth)
-                                        }
-                                    })
-                                
-                            )
-                        
-                        Circle()
-                            .fill(Color.black)
-                            .frame(width: 18, height: 18)
-                            .offset(x: self.width1)
-                            .gesture(
-                            
-                                DragGesture()
-                                    .onChanged({
-                                        (value) in
-                                        
-                                        if value.location.x<=self.totalWidth && value.location.x>=self.width{
-                                            self.width1 = value.location.x
-                                            self.filter.maxDegree = Double(100 * self.width1 / self.totalWidth)
-                                        }
-                                    })
-                                
-                            )
-                    }
-                
-                }.padding(.top, 25)
-                
-                }
-            .padding()
         }
         }
         .onAppear() {
@@ -207,26 +206,20 @@ struct CardView: View{
     var body : some View{
         
         HStack{
-             
-            Text(filter.title)
-                .fontWeight(.semibold)
-                .foregroundColor(Color.black.opacity(0.7))
-                .frame(height: 30, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-            
-            Spacer()
-            
+
             ZStack{
                 Circle()
-                    .stroke(filter.checked ? Color("green") :Color.gray, lineWidth: 1)
+                    .stroke(filter.checked ? Color(filter.color) :Color(filter.color), lineWidth: 5)
                     .frame(width: 25, height: 25)
                 
                 if filter.checked{
                     Image(systemName: "checkmark.circle.fill")
-                        .font(.system(size: 25))
-                        .foregroundColor(Color("green"))
+                        .font(.system(size: 24))
+                        .foregroundColor(Color(filter.color))
                 }
                 
             }
+
             
         }.padding(.horizontal)
         .contentShape(Rectangle())
@@ -243,6 +236,7 @@ struct FilterItem : Identifiable{
     var id = UUID().uuidString
     var title: String
     var checked: Bool
+    var color: String
     
 }
 
